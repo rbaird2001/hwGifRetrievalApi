06e8dbe32255ceb7c119ff4633188c1a29581f7d
rb2001 <rbaird2001@yahoo.com>
2019-08-06 17:28:39 -0600

    buttons working and gifs loading

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,61 @@
-var queryUrl = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=b1rMOopBhHEZCR3Thcpk9lhZzicm545Y&limit=5";
-var responseDetail;
-$.ajax({
-    url: queryUrl,
-    method: "GET"}).then(function(response){
-    responseDetail = response
-    console.log(responseDetail)});
+var favs = ["Pizza", "Doughnuts", "Sushi", "Hamburgers", "Beer"];
+var qKey = "b1rMOopBhHEZCR3Thcpk9lhZzicm545Y";
+var qLimit = 5;
+
+//
+//&limit=
+
+loadFavs();
+function loadFavs() {
+    for (i = 0; i < favs.length; i++) {
+        let divFav = $("#favorites");
+        let btnFav = $("<button>");
+        btnFav.attr("val", favs[i].toLowerCase());
+        btnFav.html(favs[i]);
+        btnFav.click(function () {
+            searchFav = $(this)
+            searchGiphy(searchFav.attr("val"));
+        })
+        divFav.append(btnFav);
+    }
+}
+
+function searchGiphy(qTerm) {
+    let queryUrl = "https://api.giphy.com/v1/gifs/search?q=";
+    queryUrl += qTerm;
+    queryUrl += "&api_key="  + qKey;
+    queryUrl += "&limit=" + qLimit;
+    
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        var resDet = response.data
+        var divResponse = $("#results")
+        divResponse.empty();
+        for (i = 0; i < resDet.length; i++) {           
+            let gifImage = $("<img>")
+            gifImage.attr({
+                "src": resDet[i].images.fixed_height_still.url,
+                "data-state": "still",
+                "data-still": resDet[i].images.fixed_height_still.url,
+                "data-move": resDet[i].images.fixed_height.url
+            }).click(function () {
+                clik = $(this)
+                if (clik.attr("data-state") === "still") {
+                    clik.attr("src", clik.attr("data-move"));
+                    clik.attr("data-state", "move");
+                }
+                else {
+                    clik.attr("src", clik.attr("data-still"));
+                    clik.attr("data-state", "still");
+                }
+            });
+            let gifRating = $("<p>").html(resDet[i].rating);
+            divResponse.append(gifRating);
+            divResponse.append(gifImage);
+        }
+
+    });
+}
+
