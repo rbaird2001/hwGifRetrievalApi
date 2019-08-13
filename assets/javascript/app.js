d3f4de0ff1c51f6a73617acb0ea98554fa4d10ff
RBaird2001 <rbaird2001@yahoo.com>
2019-08-13 02:13:01 -0600

    code cleanup. pseudocode added. sizing challenges fixed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,46 @@
 
+//api query parameters
 var qKey = "b1rMOopBhHEZCR3Thcpk9lhZzicm545Y";
 var qLimit = 10;
 
-//On load, determine window width to respond to resizing window.
-var lastWidth = $(window).width();
 
-//prepare default favorites and setup new favorites
+//prepare and load default favorites
 var favs = ["Pizza", "Doughnuts", "Sushi", "Hamburgers", "Beer"];
 loadFavs();
-newFavorite();
+setFavDispBehavior();
 
-//for mobile responsiveness and dynamic favorite list.
-collapseSize();
-$("#favToggle").click(collapseSize)
+//adjust favs display area based on collapsed or not.
+$("#favToggle").click(function () {
+    if ($("#favToggle").hasClass("collapsed")) {
+        collapseSize("expand");
+        $("#favToggle").text("Hide Favorites");
+    }
+    else {
+        collapseSize("collapse");
+        $("#favToggle").text("Show Favorites");
+    }
+})
 
-//loads the favories to the html called at various points in the app
+//Add new favorites and display all favs if collapsed.
+$("#addFav").click(function () {
+    let favName = $("#favName").val().trim();
+    if (!favName) {
+        alert("Specify your favorite first");
+        return false;
+    }
+    else {
+        let showVal = "show"
+        favs.push(favName);
+        $("#favorites").empty();
+        $("#favName").val("")
+        loadFavs(showVal);
+        collapseSize("expand")
+        $("#favToggle").removeClass("collapsed").attr("aria-expanded", true).text("Hide Favorites");
+    }
+})
+
+//loads the list of favories to the html. 
+//runs at windows load and called when new favorite added.
 function loadFavs(showVal) {
     for (i = 0; i < favs.length; i++) {
         let divFav = $("#favorites");
@@ -29,26 +55,6 @@ function loadFavs(showVal) {
         })
         divFav.append(btnFav);
     }
-    setGridRowForSizeBrkPt();
- 
-}
-
-//setup ability for adding new favorites to button list
-function newFavorite() {
-    $("#addFav").click(function () {
-        let favName = $("#favName").val().trim();
-        if (!favName) {
-            alert("Specify your favorite first");
-            return false;
-        }
-        else {
-            let showVal = "show"
-            favs.push(favName);
-            $("#favorites").empty();
-            $("#favName").val("")
-            loadFavs(showVal);
-        }
-    })
 }
 
 //build the giphy API and search
@@ -59,8 +65,7 @@ function searchGiphy(qTerm) {
     queryUrl += "&limit=" + qLimit;
     queryUrl += "&rating=pg";
 
-//execute search and return results to html
-//note that attrs use JSON type notation for all the attributes added.
+    //execute search and return results to html
     $.ajax({
         url: queryUrl,
         method: "GET"
@@ -88,49 +93,50 @@ function searchGiphy(qTerm) {
                     clik.attr("data-state", "still");
                 }
             });
-            //let gifRating = $("<span>").html(resDat[i].rating);
             divCont.append(gifImage)
             divCont.append(divRating);
             divResults.append(divCont);
         }
-
     });
 }
 
-//app uses grid area templates. 
-//Grid row must account for dynamic number favorites in list when in mobile size.
-function setGridRowForSizeBrkPt() {
+//The following three functions determine sizing and favorites display based on window size.
+
+//Window size at load will determine setting of the favorites toggle. 
+function setFavDispBehavior() {
     let w = $(window).width();
+    //if mobile, collapse favorites when loaded.
     if (w < 500) {
-        //designed so favorites are showing and you are increasing
-        //your window size, don't collapse them. Otherwise collapse.
-        // if (w > lastWidth && !$("#favToggle").hasClass("collapsed")) {
-        //     lastWidth = w;
-        //     return;
-        // }
-        $("#favToggle").addClass("collapsed").attr("aria-expanded", true);
-        $(".favButton").removeClass("show");
-       // $(".favButton").addClass("show");
-        lastWidth = w;
-        collapseSize();
+        $("#favToggle").addClass("collapsed").attr("aria-expanded", false);
+        collapseSize("collapse");
     }
-    //if not mobile sized, just go with static settings.
+    //if not mobile sized, display favorites automatically
     else {
-        $("#mainContainer").css("grid-template-rows", "80px 110px");
         $("#favToggle").removeClass("collapsed").attr("aria-expanded", true);
         $(".favButton").addClass("show");
-        lastWidth = w;
+        collapseSize("expand")
     }
 }
 
-//calculate grid row sized based on buttons being collapsed or expanded.
-function collapseSize() {
-        if (!$("#favToggle").hasClass("collapsed")) {
-            $("#mainContainer").css("grid-template-rows", "80px 110px 40px 1fr");
-        }
-        else {
-            let gridRowSize = (favs.length * 34);
-            $("#mainContainer").css("grid-template-rows", "80px 110px " + gridRowSize + "px 1fr");
-        }
+//calculate grid row sized based on favorites being collapsed or expanded.
+function collapseSize(action) {
+    if (action === "collapse") {
+        $("#mainContainer").css("grid-template-rows", "80px 110px 40px 1fr");
+    }
+    else if (action === "expand") {
+        let gridRowSize = (favs.length * 34);
+        $("#mainContainer").css("grid-template-rows", "80px 110px " + gridRowSize + "px 1fr");
+    }
 }
-window.addEventListener("resize", setGridRowForSizeBrkPt);
+
+//if window size is altered after loading, this will adjust display of favorites. 
+window.addEventListener("resize", function () {
+    let w = $(window).width();
+    if (w < 500) {
+        $(".favButton").removeClass("show");   
+    }
+    else {
+        $(".favButton").addClass("show");
+    }
+    setFavDispBehavior();
+});
